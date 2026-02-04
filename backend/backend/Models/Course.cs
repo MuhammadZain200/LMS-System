538a59d3fceb6a478476b5f3965b63d827bb3650
Muhammad Zain <m.zain.a.malik1@gmail.com>
2026-02-04 12:16:45 +0500

    Initial commit - backend and frontend setup

---
+++ backend/backend/Models/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace backend.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public List<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ backend/backend/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace backend.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public List<Enrollment> Enrollments { get; set; }
+    }
+}
